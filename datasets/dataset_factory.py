e85e5477328371d7457aaf71f4b9c6ed0c8e4d42
hesamaraghi <hesam.araghi@gmail.com>
2023-08-12 12:20:52 +0200

    rewrite dataset_factory for data_module

---
+++ datasets/dataset_factory.py
@@ -0,0 +1,35 @@
+from datasets.nmnist import NMNIST
+from datasets.ncaltech101 import NCALTECH101
+from datasets.nasl import NASL
+from datasets.ncars import NCARS
+
+
+def create_dataset(dataset_path = None, 
+                   dataset_name = None, 
+                   dataset_type = None,
+                   transform = None, 
+                   num_workers = 4):
+    if dataset_name == "NMNIST":
+        return NMNIST(  root = dataset_path, 
+                        name = dataset_type, 
+                        transform = transform,
+                        num_workers = num_workers)
+    elif dataset_name == "NCALTECH101":
+        return NCALTECH101( root = dataset_path,
+                            name = dataset_type,
+                            transform = transform,
+                            num_workers = num_workers)
+    elif dataset_name == "NASL":
+        return NASL(    root = dataset_path,
+                        name = dataset_type,
+                        transform = transform,
+                        num_workers = num_workers)
+    elif dataset_name == "NCARS":
+        return NCARS(   root = dataset_path,
+                        name = dataset_type,
+                        transform = transform,
+                        num_workers = num_workers)
+    # Add more dataset options here as needed
+    else:
+        raise ValueError("Invalid dataset name")
+
