862ed48ab3a2191fe67ef40060a1608e1a2febfb
hesamaraghi <hesam.araghi@gmail.com>
2023-08-14 15:44:28 +0200

    add self.edge_index to DynamicEdgeConv

--- models/dgcnn2.py
+++ models/dgcnn2.py
@@ -3,13 +3,83 @@ import torch.nn.functional as F
 from torch.nn import Linear
 from torch_geometric.nn import MLP, EdgeConv, DynamicEdgeConv, global_max_pool
 
+from typing import Callable, Optional, Union
+from torch import Tensor
+
+from torch_geometric.typing import OptTensor, PairOptTensor, PairTensor
+from torch_cluster import knn
+
+
+
+
+class DynamicEdgeConv2(DynamicEdgeConv):
+    r"""The dynamic edge convolutional operator from the `"Dynamic Graph CNN
+    for Learning on Point Clouds" <https://arxiv.org/abs/1801.07829>`_ paper
+    (see :class:`torch_geometric.nn.conv.EdgeConv`), where the graph is
+    dynamically constructed using nearest neighbors in the feature space.
+
+    Args:
+        nn (torch.nn.Module): A neural network :math:`h_{\mathbf{\Theta}}` that
+            maps pair-wise concatenated node features :obj:`x` of shape
+            `:obj:`[-1, 2 * in_channels]` to shape :obj:`[-1, out_channels]`,
+            *e.g.* defined by :class:`torch.nn.Sequential`.
+        k (int): Number of nearest neighbors.
+        aggr (str, optional): The aggregation scheme to use
+            (:obj:`"add"`, :obj:`"mean"`, :obj:`"max"`).
+            (default: :obj:`"max"`)
+        num_workers (int): Number of workers to use for k-NN computation.
+            Has no effect in case :obj:`batch` is not :obj:`None`, or the input
+            lies on the GPU. (default: :obj:`1`)
+        **kwargs (optional): Additional arguments of
+            :class:`torch_geometric.nn.conv.MessagePassing`.
+
+    Shapes:
+        - **input:**
+          node features :math:`(|\mathcal{V}|, F_{in})` or
+          :math:`((|\mathcal{V}|, F_{in}), (|\mathcal{V}|, F_{in}))`
+          if bipartite,
+          batch vector :math:`(|\mathcal{V}|)` or
+          :math:`((|\mathcal{V}|), (|\mathcal{V}|))`
+          if bipartite *(optional)*
+        - **output:** node features :math:`(|\mathcal{V}|, F_{out})` or
+          :math:`(|\mathcal{V}_t|, F_{out})` if bipartite
+    """
+    def __init__(self, nn: Callable, k: int, aggr: str = 'max',
+                 num_workers: int = 1, **kwargs):
+        super().__init__(nn=nn, k=k, aggr=aggr, num_workers=num_workers, **kwargs)
+
+    def forward(
+            self, x: Union[Tensor, PairTensor],
+            batch: Union[OptTensor, Optional[PairTensor]] = None) -> Tensor:
+        # type: (Tensor, OptTensor) -> Tensor  # noqa
+        # type: (PairTensor, Optional[PairTensor]) -> Tensor  # noqa
+
+        if isinstance(x, Tensor):
+            x: PairTensor = (x, x)
+
+        if x[0].dim() != 2:
+            raise ValueError("Static graphs not supported in DynamicEdgeConv")
+
+        b: PairOptTensor = (None, None)
+        if isinstance(batch, Tensor):
+            b = (batch, batch)
+        elif isinstance(batch, tuple):
+            assert batch is not None
+            b = (batch[0], batch[1])
+
+        self.edge_index = knn(x[0], x[1], self.k, b[0], b[1]).flip([0])
+
+        # propagate_type: (x: PairTensor)
+        return self.propagate(self.edge_index, x=x, size=None)
+
+
 
 class Net(torch.nn.Module):
     def __init__(self, cfg):
         super().__init__()
         self.k = cfg.model.k
         self.aggr = cfg.model.aggr
-        self.conv1 = DynamicEdgeConv(MLP([2 * 4, 64, 64, 64]), self.k, self.aggr)
+        self.conv1 = DynamicEdgeConv2(MLP([2 * 4, 64, 64, 64]), self.k, self.aggr)
         # self.conv2 = DynamicEdgeConv(MLP([2 * 64, 128]), k, aggr)
         self.conv2 = EdgeConv(MLP([2 * 64, 128]), self.aggr)
         self.lin1 = Linear(128 + 64, 1024)
