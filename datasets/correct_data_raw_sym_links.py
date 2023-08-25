e9ee4ec708746272cf8efceda7d7c9798d23e79b
hesamaraghi <hesam.araghi@gmail.com>
2023-08-25 11:33:19 +0200

    modify for NCARS with list of folders

--- datasets/correct_data_raw_sym_links.py
+++ datasets/correct_data_raw_sym_links.py
@@ -19,32 +19,69 @@ import numpy as np
 # %%
 
 download_folder = {'NCALTECH101':   'Caltech101',
-                   'NASL':          ''}
+                   'NASL':          '',
+                   'NCARS':         ['n-cars_test', 'n-cars_train']}
 
 def correct_sym_links(dataset_name):
     assert dataset_name in download_folder.keys(), f"{dataset_name} is not in {download_folder.keys()}"
     raw_path = osp.join(osp.dirname(osp.abspath(__file__)),dataset_name,'data','raw')
-    src_path = osp.join(osp.dirname(osp.abspath(__file__)),dataset_name,'downloaded',download_folder[dataset_name])
-    assert osp.exists(src_path), f"'download' directory for {dataset_name} dataset is corrupted!"
     all_path = os.path.join(raw_path, 'all')
+    if isinstance(download_folder[dataset_name],list):  
+        src_path_list = [osp.join(osp.dirname(osp.abspath(__file__)),dataset_name,'downloaded',folder) for folder in download_folder[dataset_name]]   
+        assert all([osp.exists(src_path) for src_path in src_path_list]), f"'download' directory for {dataset_name} dataset is corrupted!"
+
+    ## creating the 'all' folder
+        
+        if osp.exists(all_path):
+            shutil.rmtree(all_path)
+        os.mkdir(all_path)
+        
+        for src_path in src_path_list:
+            for f in glob(os.path.join(src_path, '*','*')):
+                class_name = f.split(os.sep)[-2]
+                if not osp.exists(os.path.join(all_path, class_name)):
+                    os.mkdir(os.path.join(all_path, class_name))
+                dst_path = os.path.join(all_path, class_name, f.split(os.sep)[-1])
+                try:
+                    os.symlink(f,dst_path)
+                except FileExistsError:
+                    os.unlink(dst_path)
+                    os.symlink(f,dst_path)
+                    
+        for folder in ['training', 'validation', 'test']:
+            for f in glob(os.path.join(raw_path, folder, '*','*')):
+                all_corresponding_path = osp.join(all_path,*f.split(os.sep)[-2:])
+                src = os.readlink(all_corresponding_path)
+                try:
+                    os.symlink(src,f)
+                except FileExistsError:
+                    os.unlink(f)
+                    os.symlink(src,f)
+                    
+    elif isinstance(download_folder[dataset_name],str):
+        src_path = osp.join(osp.dirname(osp.abspath(__file__)),dataset_name,'downloaded',download_folder[dataset_name])
+        assert osp.exists(src_path), f"'download' directory for {dataset_name} dataset is corrupted!"
+        try:
+            os.symlink(src_path,dst_path)
+        except FileExistsError:
+            os.unlink(dst_path)
+            os.symlink(src_path,dst_path)
+            
+        for folder in ['training', 'validation', 'test']:
+            for f in glob(os.path.join(raw_path, folder, '*','*')):
+                src = osp.join(src_path,*f.split(os.sep)[-2:])
+                try:
+                    os.symlink(src,f)
+                except FileExistsError:
+                    os.unlink(f)
+                    os.symlink(src,f)
+            
+    else:
+        raise "download_folder values should be a list of strings or a string!"
 
     # %%
-    try:
-        os.symlink(src_path,all_path)
-    except FileExistsError:
-        os.unlink(all_path)
-        os.symlink(src_path,all_path)
 
-    # %%
 
-    for folder in ['training', 'validation', 'test']:
-        for f in glob(os.path.join(raw_path, folder, '*','*')):
-            src = osp.join(src_path,*f.split(os.sep)[-2:])
-            try:
-                os.symlink(src,f)
-            except FileExistsError:
-                os.unlink(f)
-                os.symlink(src,f)
 
 
 if __name__ == '__main__':
