4f2e488912db8cc99747c1fe0bb3f701fd10a157
hesamaraghi <hesam.araghi@gmail.com>
2023-12-06 17:06:25 +0100

    Merge branch 'main' of github.com:hesamaraghi/DGCNN_CV_format into multi-val-test

---
+++ clear_DGCNN_checkpoints.py
@@ -0,0 +1,53 @@
+from glob import glob
+import os
+import argparse
+import wandb
+import shutil
+
+
+def main():
+    api = wandb.Api()
+
+    runs = api.runs("haraghi/sweep EST (FAN1VS3)")
+    valid_run_ids = [r.id for r in runs]
+    checkpoint_folders = glob(os.path.join(args.folder, '*'))
+
+    empty_folders = []
+    for folder in checkpoint_folders:
+        if not os.listdir(folder):
+            empty_folders.append(folder)
+
+    print("Empty folders:")
+    print(empty_folders)
+
+    for folder in empty_folders:
+        os.rmdir(os.path.join(args.folder, folder))
+            
+    not_in_wandb = []
+    for folder in checkpoint_folders:
+        folder = folder.split(os.sep)[-1]
+        if folder not in valid_run_ids:
+            not_in_wandb.append(folder)
+            
+    print("Not in wandb:")
+    print(not_in_wandb)
+    for folder in not_in_wandb:
+        shutil.rmtree(os.path.join(args.folder, folder))
+        print(f"Removed {folder}")
+
+
+    
+
+    
+    
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Your script description.")
+
+    parser.add_argument("--folder", type=str, default="DGCNN", help="folder to clear")
+    args = parser.parse_args()
+
+    main()
---
+++ submit_bunch_sweep.py
@@ -0,0 +1,30 @@
+import subprocess
+
+import os
+import argparse
+
+def main():
+
+    for _ in range(args.num_run):
+        
+        sbatch_command = f"sbatch {args.sbatch_folder}/run_train.sbatch wandb agent --count {args.agent_count} {args.sweep_id}"
+        print(sbatch_command)
+        # Submit the job using subprocess
+        # subprocess.call(sbatch_command, shell=True)
+
+if __name__ == "__main__":
+    
+    # Define the paths
+    os.environ["WANDB_CACHE_DIR"] = os.path.join('.', 'cache')
+    
+    parser = argparse.ArgumentParser(description="Your script description.")
+    
+    parser.add_argument("-n", "--num-run", type=int, default=1, help="number of runs")
+    parser.add_argument("--sbatch-folder", type=str, default="sbatch_folder")
+    parser.add_argument("--agent-count", type=int, default=1, help="number of agents per call")
+    parser.add_argument("--sweep-id", type=str, help="sweep id")
+    parser.add_argument("--num-call", type=int, default=1, help="number of calls of the agent in each run")
+
+    args = parser.parse_args()
+
+    main()
