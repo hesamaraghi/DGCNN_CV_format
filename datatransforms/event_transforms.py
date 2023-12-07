d0dba939fe2e562fae8de26b0aa025a9fb1081e5
hesamaraghi <hesam.araghi@gmail.com>
2023-12-07 14:33:23 +0100

    add VaryingSamplingPoints class

--- datatransforms/event_transforms.py
+++ datatransforms/event_transforms.py
@@ -3,7 +3,8 @@ import os.path as osp
 import scipy.io as sio
 import re
 import random
-
+import math
+import numpy as np
 import torch
 from torch_geometric.transforms import BaseTransform
 from torch_geometric.data import Data, HeteroData
@@ -204,3 +205,62 @@ class ShiftAndFlip(BaseTransform):
     def __repr__(self) -> str:
         return (f'{self.__class__.__name__}(max_shift={self.max_shift}, '
                 f'p={self.p})')
+        
+        
+        
+class VaryingSamplingPoints(BaseTransform):
+    r"""Samples a random number of points and features from the nodes.
+    Args:
+        range_num (tuple): minimun and maximum for random number of points to sample.
+        replace (bool, optional): If set to :obj:`False`, samples points
+            without replacement. (default: :obj:`True`)
+        allow_duplicates (bool, optional): In case :obj:`replace` is
+            :obj`False` and :obj:`num` is greater than the number of points,
+            this option determines whether to add duplicated nodes to the
+            output points or not.
+            In case :obj:`allow_duplicates` is :obj:`False`, the number of
+            output points might be smaller than :obj:`num`.
+            In case :obj:`allow_duplicates` is :obj:`True`, the number of
+            duplicated points are kept to a minimum. (default: :obj:`False`)
+    """
+    def __init__(
+        self,
+        range_num: tuple,
+        replace: bool = True,
+        allow_duplicates: bool = False,
+    ):
+        self.range_num = range_num
+        self.replace = replace
+        self.allow_duplicates = allow_duplicates
+
+    def __call__(self, data: Data) -> Data:
+        
+        min_num, max_num = self.range_num
+        self.num = random.randint(min_num, max_num)
+        
+        num_nodes = data.num_nodes
+
+        if self.replace:
+            choice = np.random.choice(num_nodes, self.num, replace=True)
+            choice = torch.from_numpy(choice).to(torch.long)
+        elif not self.allow_duplicates:
+            choice = torch.randperm(num_nodes)[:self.num]
+        else:
+            choice = torch.cat([
+                torch.randperm(num_nodes)
+                for _ in range(math.ceil(self.num / num_nodes))
+            ], dim=0)[:self.num]
+
+        for key, item in data:
+            if key == 'num_nodes':
+                data.num_nodes = choice.size(0)
+            elif bool(re.search('edge', key)):
+                continue
+            elif (torch.is_tensor(item) and item.size(0) == num_nodes
+                  and item.size(0) != 1):
+                data[key] = item[choice]
+
+        return data
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}(({self.range_num[0]},{self.range_num[1]}), replace={self.replace})'
