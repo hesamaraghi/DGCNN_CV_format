1472cb4fedf4982323b27162530ae2f776a219c9
hesamaraghi <hesam.araghi@gmail.com>
2023-12-07 14:34:39 +0100

    add VaryingSamplingPoints

--- transform_factory.py
+++ transform_factory.py
@@ -1,5 +1,5 @@
 import torch_geometric.transforms as T
-from datatransforms.event_transforms import TemporalScaling, RemoveOutliers, SpatialCentering, SpatialScaling, AddEdgeAttr, ShiftAndFlip, FilterNodes
+from datatransforms.event_transforms import *
 
 def factory(cfg):
       
@@ -17,6 +17,10 @@ def factory(cfg):
                   transform_list.append(SpatialCentering())
             if cfg.num_events_per_sample is not None:
                   transform_list.append(T.FixedPoints(cfg.num_events_per_sample, replace = False, allow_duplicates = True))
+            elif cfg.random_num_events_per_sample.transform:
+                  transform_list.append(VaryingSamplingPoints((   cfg.random_num_events_per_sample.min_num_events, 
+                                                                  cfg.random_num_events_per_sample.max_num_events), 
+                                                                  replace = False, allow_duplicates = False)) # TODO: allow duplicates is set to False for now
             if cfg.random_flip is not None:
                   transform_list.append(T.RandomFlip(axis = 0, p = cfg.random_flip))
             if cfg.scale_limits is not None:
