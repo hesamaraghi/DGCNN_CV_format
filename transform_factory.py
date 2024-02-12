2ab89245bbcd27655afb4224c1c18ad8f0b349a4
hesamaraghi <hesam.araghi@gmail.com>
2024-02-12 10:04:37 +0100

    convert cfg to dict to check if cfg fields exist

--- transform_factory.py
+++ transform_factory.py
@@ -1,37 +1,38 @@
 import torch_geometric.transforms as T
 from datatransforms.event_transforms import *
+from omegaconf import OmegaConf
 
 def factory(cfg):
-      
-      if cfg.transform:
+      cfg_dict = OmegaConf.to_object(cfg)
+      if 'transform' in cfg_dict and cfg.transform:
             transform_list = []
-            if cfg.filter_nodes is not None:
+            if 'filter_nodes' in cfg_dict and cfg.filter_nodes is not None:
                   transform_list.append(FilterNodes(cfg))
-            if cfg.thresh_quantile is not None:
+            if 'thresh_quantile' in cfg_dict and cfg.thresh_quantile is not None:
                   transform_list.append(RemoveOutliers(cfg))
-            if cfg.shift_and_flip.transform:
+            if 'shift_and_flip' in cfg_dict and cfg.shift_and_flip.transform:
                   transform_list.append(ShiftAndFlip(cfg.shift_and_flip))
-            if cfg.temporal_scale is not None:
+            if 'temporal_scale' in cfg_dict and cfg.temporal_scale is not None:
                   transform_list.append(TemporalScaling(cfg))
-            if cfg.spatial_centering:
+            if 'spatial_centering' in cfg_dict and cfg.spatial_centering:
                   transform_list.append(SpatialCentering())
-            if cfg.num_events_per_sample is not None:
+            if 'num_events_per_sample' in cfg_dict and cfg.num_events_per_sample is not None:
                   transform_list.append(T.FixedPoints(cfg.num_events_per_sample, replace = False, allow_duplicates = True))
-            elif cfg.random_num_events_per_sample.transform:
+            elif 'random_num_events_per_sample' in cfg_dict and cfg.random_num_events_per_sample.transform:
                   transform_list.append(VaryingSamplingPoints((   cfg.random_num_events_per_sample.min_num_events, 
                                                                   cfg.random_num_events_per_sample.max_num_events), 
                                                                   replace = False, 
                                                                   allow_duplicates = False,
                                                                   inverse_sampling = cfg.random_num_events_per_sample.inverse_sampling)) # TODO: allow duplicates is set to False for now
-            if cfg.random_flip is not None:
+            if 'random_flip' in cfg_dict and cfg.random_flip is not None:
                   transform_list.append(T.RandomFlip(axis = 0, p = cfg.random_flip))
-            if cfg.scale_limits is not None:
+            if 'scale_limits' in cfg_dict and cfg.scale_limits is not None:
                   transform_list.append(SpatialScaling(cfg))
-            if cfg.degree_limit is not None:
+            if 'degree_limit' in cfg_dict and cfg.degree_limit is not None:
                   transform_list.append(T.RandomRotate(cfg.degree_limit, axis = 2))
-            if cfg.radius_graph.transform:
+            if 'radius_graph' in cfg_dict and cfg.radius_graph.transform:
                   transform_list.append(T.RadiusGraph(r = cfg.radius_graph.r,max_num_neighbors = cfg.radius_graph.max_num_neighbors))
-            if cfg.radius_graph.add_edge_attr.transform:
+            if 'radius_graph' in cfg_dict and cfg.radius_graph.add_edge_attr.transform:
                   transform_list.append(AddEdgeAttr(cfg.radius_graph.add_edge_attr))
             return T.Compose(transform_list)
       else:
