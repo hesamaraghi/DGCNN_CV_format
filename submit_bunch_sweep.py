8c1ff239202b5d2944dd0170a7bc2842eed602d3
hesamaraghi <hesam.araghi@gmail.com>
2024-07-05 17:11:08 +0200

    convert sbatch_folder to sbatch_file for submit_bunch_sweep.py

--- submit_bunch_sweep.py
+++ submit_bunch_sweep.py
@@ -7,7 +7,7 @@ def main():
 
     for _ in range(args.num_run):
         
-        sbatch_command = f"sbatch {args.sbatch_folder}/run_train.sbatch wandb agent --count {args.agent_count} {args.sweep_id}"
+        sbatch_command = f"sbatch {args.sbatch_file} wandb agent --count {args.agent_count} {args.sweep_id}"
         print(sbatch_command)
         # Submit the job using subprocess
         subprocess.call(sbatch_command, shell=True)
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Your script description.")
     
     parser.add_argument("-n", "--num-run", type=int, default=1, help="number of runs")
-    parser.add_argument("--sbatch-folder", type=str, default="sbatch_folder")
+    parser.add_argument("--sbatch-file", type=str)
     parser.add_argument("--agent-count", type=int, default=1, help="number of agents per call")
     parser.add_argument("--sweep-id", type=str, help="sweep id")
  
