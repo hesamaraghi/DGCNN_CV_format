57659e27e659f364318b8f984959e78723513cd4
hesamaraghi <hesam.araghi@gmail.com>
2024-12-31 08:46:39 +0100

    Add put_event_side_by_side function for combining PyG datasets

--- utils/visualize_utils.py
+++ utils/visualize_utils.py
@@ -2,6 +2,7 @@ from matplotlib import animation
 import numpy as np 
 import matplotlib.pyplot as plt
 from utils.data_utils import is_numpy_event_data, is_pyg_event_data, pyg2numpy_event_convertor
+import torch
 
 def animate_events(events, transform, fig_size=None):
     
@@ -26,7 +27,7 @@ def animate_events(events, transform, fig_size=None):
     im = ax.imshow(frames[0])
     # plt.axis("off")
     # plt.axis("tight")
-
+    # ax.invert_yaxis()
     def animate(frame):
         im.set_data(frame)
         return [im]
@@ -36,3 +37,17 @@ def animate_events(events, transform, fig_size=None):
 
     return anim
         
+def put_event_side_by_side(datas, image_size, axis = 0):
+    assert isinstance(datas, list), "The input data must be a list of PyG datasets."
+    assert len(datas) > 1, "The input data must contain at least two PyG datasets."
+    num_data = len(datas)
+    data_combined = datas[0].clone()
+    for i in range(1,num_data):  
+        data_to_be_shifted = datas[i].clone()  
+        data_to_be_shifted.pos[:,axis] += i * image_size[axis]
+        data_combined.pos = torch.cat([data_combined.pos, data_to_be_shifted.pos], dim=0)
+        data_combined.x = torch.cat([data_combined.x, data_to_be_shifted.x], dim=0)
+    sorted_indices = torch.argsort(data_combined.pos[..., -1])
+    data_combined.pos = data_combined.pos[sorted_indices]
+    data_combined.x = data_combined.x[sorted_indices]
+    return data_combined
